24b326b58cb0ccd08331e4fd7066d7ee78ec7061
Eben <me@ebenroux.co.za>
2017-04-02 15:44:13 +0200

    - v1.0.1

--- Shuttle.Esb.Tools.TransferMessages/Shuttle.Esb.Tools.TransferMessages/Properties/AssemblyInfo.cs
+++ Shuttle.Esb.Tools.TransferMessages/Shuttle.Esb.Tools.TransferMessages/Properties/AssemblyInfo.cs
@@ -1,36 +1,26 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Shuttle.Esb.Tools.TransferMessages")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+#if NET451FULL
+[assembly: AssemblyTitle("Shuttle.Esb.Tools.TransferMessages for .NET Framework 4.5.1")]
+#endif
+
+#if NET452FULL
+[assembly: AssemblyTitle("Shuttle.Esb.Tools.TransferMessages for .NET Framework 4.5.2")]
+#endif
+
+#if NET46FULL
+[assembly: AssemblyTitle("Shuttle.Esb.Tools.TransferMessages for .NET Framework 4.6")]
+#endif
+
+#if NET461FULL
+[assembly: AssemblyTitle("Shuttle.Esb.Tools.TransferMessages for .NET Framework 4.6.1")]
+#endif
+
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyCopyright("Copyright © Eben Roux 2017")]
 [assembly: AssemblyProduct("Shuttle.Esb.Tools.TransferMessages")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyCompany("Shuttle")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4f963f69-9cd7-4b4d-a779-d3cbef8be82c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
